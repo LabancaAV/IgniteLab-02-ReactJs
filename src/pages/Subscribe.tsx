90aa7f1c47ce9aa9945a91a4b314f3402e75bf82
LabancaAV <andrelabanca2@gmail.com>
2022-07-26 20:40:45 -0300

    feat: adicionando responsividade à pagina Subscribe.

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -28,18 +28,20 @@ export function Subscribe(){
 
   return(
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+      <div className="w-full max-w-[1100px] flex flex-col md:flex-row items-center justify-between mt-20 mx-auto">
         <div className="max-w-[640px]">
-          <Logo/>
-          <h1 className="mt-8 text-[2.5rem] leading-tight">
+          <div className="flex justify-center md:justify-start">
+            <Logo/>
+          </div>
+          <h1 className="mt-8 md:px-0 px-8 md:text-[2.5rem] text-[1.5rem] leading-tight md:text-left text-center">
             Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React JS</strong>
           </h1>
-          <p className="mt-4 text-gray-200 leading-relaxed">
+          <p className="md:mt-4 mt-8 md:mb-0 mb-8 md:px-0 px-8 md:text-[1rem] text-[0.8rem] text-gray-200 leading-relaxed md:text-left text-center">
             Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
           </p>
         </div>
-        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
-          <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
+        <div className="p-8 bg-gray-700 border border-gray-500 rounded md:w-96 w-full">
+          <strong className="md:text-2xl text-1xl mb-6 block">Inscreva-se gratuitamente</strong>
 
           <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
             <input 
@@ -66,7 +68,7 @@ export function Subscribe(){
         </div>
       </div>
 
-      <img src={logoMockup} alt="mt-10" />
+      <img src={logoMockup} alt="mt-10" className="md:p-0 px-4"/>
     </div>
     )
 }
